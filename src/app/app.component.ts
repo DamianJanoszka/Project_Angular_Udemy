661459ac6be57f4851e92bce0ee1c327be4868d4
d.janoszka <d.janoszka@wasko.pl>
2022-01-25 14:08:42 +0100

    Seventh exercise

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,23 @@
-import { Component, ViewChild  } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { Component, OnInit} from '@angular/core';
+import { FormGroup, Validators, FormControl } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'my-first-app';
+  projectForm: FormGroup;
 
-  @ViewChild('myForm', { static: false }) signupForm: NgForm;
-  selectedLevel = 'Pro';
+  ngOnInit(){
+    this.projectForm = new FormGroup({
+      'projectName': new FormControl(null, Validators.required),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('critical')
 
-  onSubmit() {
-    console.log(this.signupForm.value);
+    });
+  }
+  onSubmit(){
+    console.log(this.projectForm.value)
   }
 }
